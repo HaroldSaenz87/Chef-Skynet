0e201ebe69714400722f0db4f4ea08432e447f85
Harold Saenz <joshu4nick19@gmail.com>
2025-09-16 21:14:42 -0400

    adding useState and prevent browser page reload

--- SkynetChef/src/Middle.jsx
+++ SkynetChef/src/Middle.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
+
 function Middle(){
+
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsList = ingredients.map(ingredients => (
+        <li key={ingredients}>{ingredients}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return(
         <main>
-            <form className="ingred-form">
+            <form onSubmit={handleSubmit} className="ingred-form">
                 <input type="text"
-                placeholder="e.g. oregano" aria-label="Add ingredient" />
+                placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient" />
                 <button>+ Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientsList}
+            </ul>
+
         </main>
     )
 }
